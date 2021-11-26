b45a3bd13c15e3de8c5d7e9802c0e67e1f964bbc
sungwoon95 <lad0305@naver.com>
2021-11-26 18:16:50 +0900

    feat: Make the registered menu editable

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+
+    const updateCount = () => {
+        const menuCount = $("#coffee-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount}개`;
+    }
+
+    $("#coffee-list").addEventListener("click", (e) => {
+        if(e.target.classList.contains("edit-button")){
+            const $menuName = e.target.closest("li").querySelector(".menu-name") 
+            const menuName = $menuName.innerText;
+            const updatedMenu = prompt("메뉴명을 수정하세요", menuName);
+            
+            $menuName.innerText = updatedMenu;
+        }
+    })
+
     // 폼 태그 자동 전송 방지
     $("#coffee-form")
         .addEventListener("submit", (e) => {
@@ -16,14 +32,24 @@ function App() {
         
         const coffeeName = $("#coffee-name").value;
         const menuTemplate = (coffeeName) => {
-            return `<li>${coffeeName}</li>`
+            return `
+            <li>
+                <p class= "menu-name">
+                    ${coffeeName}
+                </p>
+                <button class="edit-button">
+                    수정
+                </button>
+                <button class="delete-button">
+                    삭제
+                </button>
+            </li>`
         };
         $("#coffee-list").insertAdjacentHTML(
             "beforeend", menuTemplate(coffeeName)
         );
         //$("#coffee-list").innerHTML = menuTemplate(coffeeName);
-        const menuCount = $("#coffee-list").querySelectorAll("li").length;
-        $(".menu-count").innerText = `총 ${menuCount}개`;
+        updateCount();
         $("#coffee-name").value = "";
     }
 
@@ -35,7 +61,7 @@ function App() {
             if (e.key !== "Enter"){
                 return;
             }
-                coffeeSubmit();
+            coffeeSubmit();
         });
 }
 
